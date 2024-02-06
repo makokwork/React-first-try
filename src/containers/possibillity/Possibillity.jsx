31cb8f48ebf34e4083252a1659d2e7f391271771
makokwork <frdopoesd@yandex.ru>
2024-02-06 19:37:13 +0300

    aboabus

---
+++ src/containers/possibillity/Possibillity.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './possibillity.css'
+import possibilityImage from '../../assets/possibility.png'
+const Possibillity = () => {
+  return (
+	<div className='gpt3__possibillity secion__padding' id="possibillity">
+		<div className='gpt3__possibillity-image'>
+			<img src={possibilityImage} alt="image" />
+		</div>
+		<div className='gpt3__possibillity-content'>
+		<h4>Request Early Acess to Get Started</h4>
+		<h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+		<p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+		<h4>Request Early Access to Get Started</h4>
+		</div>
+	  
+	</div>
+  )
+}
+
+export default Possibillity
